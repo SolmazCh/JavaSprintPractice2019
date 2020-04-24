704eb557e46ce95c71be5811ac69fadf67ba3c1c
SolmazCh <solmaz.chiraqova@gmail.com>
2020-04-24 07:31:47 -0400

    add HelloWorld.java

---
+++ src/day27_DateTime/Dates_Practice.java
@@ -0,0 +1,28 @@
+package day27_DateTime;
+
+import java.time.LocalDate;
+
+public class Dates_Practice {
+
+    public static void main(String[] args) {
+
+        LocalDate date1 = LocalDate.of(1985,05,4);
+
+        System.out.println(date1);
+
+        LocalDate myBirthday = LocalDate.of(1990, 8,3);
+
+        System.out.println(myBirthday);
+
+        LocalDate.now();
+
+
+
+
+
+
+
+
+
+    }
+}
